96747f786853c02137a07b9cbca47e8b31c0e7e4
ts moosavi <tt.musavi@gmail.com>
2022-05-08 00:26:01 +0430

    add database

---
+++ app/src/main/java/com/example/hw16_cities/CityDatabase.kt
@@ -0,0 +1,23 @@
+package com.example.hw16_cities
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [CityEntity::class],version = 1)
+abstract class CityDatabase:RoomDatabase() {
+    abstract fun cityDao():DaoOfCityEntity
+    companion object{
+    var     INSTANCE: CityDatabase? = null
+        fun getDatabase(context: Context): CityDatabase?{
+            if (INSTANCE == null){
+                synchronized(CityDatabase::class){
+                    INSTANCE = Room.databaseBuilder(context.applicationContext,CityDatabase::class.java,"myDb")
+                        .allowMainThreadQueries().build()
+                }
+            }
+            return INSTANCE
+        }
+    }
+}
